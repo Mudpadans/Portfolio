9b0be7da58c67876ab9a2b409fbaca61cea0efc6
Mudpadans <philmodel.101@gmail.com>
2023-11-09 22:10:25 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>
+          Hello World! I'm Jonathan Phillips
+        </h1>
+    </div>
+  );
+}
+
+export default App;
